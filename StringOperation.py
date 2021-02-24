523487ec75eec2ace91f0da22845ae26057f91c6
JamdarGauravkumarLaxman <79578334+JamdarGauravkumarLaxman@users.noreply.github.com>
2021-02-24 17:31:52 +0530

    Create StringOperation.py
    Operation related to string,code done in python language

---
+++ StringOperation.py
@@ -0,0 +1,20 @@
+def main():
+    s1="Mariam has graduated with an honor degree"
+    
+    print(s1[4])
+    print(s1[-5])
+    print(s1[0])
+    print(s1[:6])
+    print(s1[7:12])
+    print(len(s1))
+    
+if __name__=="__main__":
+    main()
+
+//output
+a
+e
+M
+Mariam
+has g
+41
